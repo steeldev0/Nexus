3bf2a9a4ca0116d985ae4cb28d079ace84a2e976
User0 <104380885+user0-tb@users.noreply.github.com>
2024-05-21 08:07:45 +0000

    Update about.py

--- src/commands/about.py
+++ src/commands/about.py
@@ -2,7 +2,8 @@
 async def about(ctx):
     embed = nextcord.Embed(
             title="About this bot:",
-            description=" ",
+            description="""Nexus is your go-to cross guild chatting Discord bot! This bot connects to all your favourite server to form a tree of chatting, enabling you to talk to others and possibly make new frends! There are lots of other servers with this bot; feel free to add it to yours! We hope you enjoy using Nexus! Happy chatting :)
+Support server : https://discord.gg/8eF2KhMS5Q""",
             color=nextcord.Color.blue()
     )
     embed.insert_field_at(0, name = f"""Operating System""", value=platform.platform(), inline=False)
