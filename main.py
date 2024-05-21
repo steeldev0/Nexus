df04c04a6ea367e79fe9d070f1dd15a1a2d41d8f
User0 <104380885+user0-tb@users.noreply.github.com>
2024-05-21 07:56:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import asyncio
 import re
 from urllib.parse import urlparse
 import threading
+import platform
 
+startTime = datetime.now()
 intents = nextcord.Intents.default()
 intents.message_content = True
 intents.guilds = True
@@ -190,4 +192,4 @@ def load_commands(directory):
 
 load_commands("src/commands")
 
-bot.run('Please insert your token here. Make sure that the "Message Content Intent" is enabled in the developer portal.')
+bot.run('Place your token here. Make sure the "Message content intent" is enabled.')
