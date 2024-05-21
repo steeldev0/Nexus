9b1f67ea5876ccba8c762d0a21909b182ee74040
User0 <104380885+user0-tb@users.noreply.github.com>
2024-05-21 08:00:10 +0000

    Update about.py

--- src/commands/about.py
+++ src/commands/about.py
@@ -1,11 +1,10 @@
 @bot.slash_command(description="Informations about the bot")
 async def about(ctx):
     embed = nextcord.Embed(
-            title="Hello! This bot is based on Nexus.",
-            description="Read the text below for more information.",
+            title="About this bot:",
+            description=" ",
             color=nextcord.Color.blue()
     )
-    embed.insert_field_at(0, name = f"""Sources""", value = "https://github.com/steeldev0/Nexus", inline=False)
-    embed.insert_field_at(1, name = f"""Operating System""", value=platform.platform(), inline=False)
-    embed.insert_field_at(3, name = f"""Bot's uptime(HH:MM:SS.MS)""", value=datetime.now() - startTime, inline=False)
+    embed.insert_field_at(0, name = f"""Operating System""", value=platform.platform(), inline=False)
+    embed.insert_field_at(1, name = f"""Bot's uptime(HH:MM:SS.MS)""", value=datetime.now() - startTime, inline=False)
     await ctx.send(embed=embed)
