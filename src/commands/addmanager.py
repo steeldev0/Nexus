8af06894d73b09cd474e81f86725a1fd17119dd0
steeldev <104603185+steeldev0@users.noreply.github.com>
2024-06-11 20:56:57 +0300

    Update addmanager.py

--- src/commands/addmanager.py
+++ src/commands/addmanager.py
@@ -1,10 +1,10 @@
 @bot.slash_command(name='addmanager')
-async def add_manager(ctx, user: nextcord.User):
-    if ctx.author.id not in owner_ids:
-        await ctx.send("You need to be an owner to use this command.")
+async def add_manager(ctx: nextcord.Interaction, user: nextcord.User):
+    if ctx.user.id not in owner_ids:
+        await ctx.send("You need to be an owner to use this command.", ephemeral=True)
         return
     
-    with open("managers.txt", "a", encoding="utf-8") as file:
-        file.write(f"{user.id}\n")
+    async with aiofiles.open("managers.txt", "a", encoding="utf-8") as file:
+        await file.write(f"{user.id}\n")
     manager_ids.append(user.id)
     await ctx.send(f"Added {user.mention} as a manager")
